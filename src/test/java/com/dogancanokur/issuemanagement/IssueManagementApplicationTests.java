609326e95c34445a67729879ad6b0e9feffd04c0
dogancanokur <ahmet.okur.94@gmail.com>
2020-11-22 00:57:34 +0300

    first commit

---
+++ src/test/java/com/dogancanokur/issuemanagement/IssueManagementApplicationTests.java
@@ -0,0 +1,11 @@
+package com.dogancanokur.issuemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class IssueManagementApplicationTests {
+
+  @Test void contextLoads() {
+  }
+
+}
