f360eeee38087f2b6c4885a4c2670545a3d48ad2
dogan.okur <dogan.okur@hititcs.com>
2020-12-19 06:08:05 +0300

    angular template and change css to scss

--- src/webui/src/app/app.module.ts
+++ src/webui/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
+import {AppLayoutComponent, FooterComponent, HeaderComponent, SidebarComponent} from "../_layout";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLayoutComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
